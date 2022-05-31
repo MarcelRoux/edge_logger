47a26b8473b62fb79bd9b5ed2a53ffe6ad40dbb2
Marcel <marcel.roux.email@gmail.com>
2022-05-31 15:42:46 -0400

    Updated setup file.

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 
 setup(name='edge_logger',
-      version='0.1',
+      version='0.0.1',
       description='An edge device data logger.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/MarcelRoux/edge_logger',
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
       author='Marcel',
       author_email='',
       license='MIT',
-      packages=['edge_logger'],
-      zip_safe=False)
+      package_dir={'': 'edge_logger'},
+      packages=find_packages(where='edge_logger'),
+      python_requires='>=3.6'
+)
