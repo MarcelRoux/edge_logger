6cd1eeee3bc066a41cf1e61179b716b5ec4dcdc7
Marcel <marcel.roux.email@gmail.com>
2022-05-31 13:41:22 -0400

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='edge_logger',
+      version='0.1',
+      description='An edge device data logger.',
+      url='https://github.com/MarcelRoux/edge_logger',
+      author='Marcel',
+      author_email='',
+      license='MIT',
+      packages=['edge_logger'],
+      zip_safe=False)
