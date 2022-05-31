15d2014181a30d7ccb3c7d246535f94fbaf0abc2
Marcel <marcel.roux.email@gmail.com>
2022-05-31 11:12:43 -0400

    Added sample table creation script.

---
+++ table_scripts.py
@@ -0,0 +1,8 @@
+TEMPERATURE_TABLE = (
+    "CREATE TABLE IF NOT EXISTS temperature ("
+    " 'id' INTEGER PRIMARY KEY AUTOINCREMENT,"
+    " 'created_on' DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%f','now'))," # UTC time.
+    " 'temp_c' REAL NOT NULL,"
+    " 'temp_f' REAL NOT NULL,"
+    ");"
+)
