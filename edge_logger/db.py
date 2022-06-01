db70c17ce061b025b3270f486ba4e0013fc7d963
Marcel <marcel.roux.email@gmail.com>
2022-06-01 17:44:23 -0400

    Added context manager capability to SensorLog.

--- edge_logger/db.py
+++ edge_logger/db.py
@@ -3,43 +3,63 @@ import os
 from sqlite3 import Connection, Cursor
 from typing import Tuple
 
-from table_scripts import TEMPERATURE_TABLE_CREATE, TEMPERATURE_TABLE_INSERT
+from table_scripts import SENSOR_TABLE_CREATE
 from time_module import now_utc, now_datetime_hour_string
 
-SENSOR = 'dht'
+SENSOR = 'sensor'
 DB_ROOT = './data'
 
 
 class SensorLog:
 
-    def __init__(self, sensor=SENSOR,
-                 table_setup_script=TEMPERATURE_TABLE_CREATE,
-                 table_insert_script=TEMPERATURE_TABLE_INSERT, db_root=DB_ROOT):
+    def __init__(self,
+                 sensor=SENSOR,
+                 table_create_script=SENSOR_TABLE_CREATE,
+                 db_root=DB_ROOT):
+        print('__init__ called')
         self.sensor = sensor
         self.db_root = db_root
-        self.table_insert_script = table_insert_script
+        self.table_create_script = table_create_script
 
         self.db_dir = self.setup_folder_structure()
-        self.conn, self.cur = self.cursor()
-        self.setup_table(table_setup_script)
+        self.db_name = self.create_db_name()
 
-    def cursor(self) -> Tuple[Connection, Cursor]:
+    def __enter__(self):
+        print('__enter__ called')
+        self.conn, self.cur = self.cursor()
+        # self.create_table()
+
+        return self.conn
+
+    def __exit__(self, type, value, traceback):
+        print('__exit__ called')
+        self.conn.commit()
+        self.conn.close()
+
+    def create_db_name(self):
+        '''
+        Sets up a database file name.
+        '''
+        return f'{self.db_dir}/{self.sensor}_{now_datetime_hour_string()}.db'
+
+    def cursor(self, db_name=None) -> Tuple[Connection, Cursor]:
         '''
         Sets up a table connection and creates a file if none exists.
         '''
-        db_name = f'{self.db_dir}/{self.sensor}_{now_datetime_hour_string()}.db'
+        if(db_name is None):
+            db_name = self.db_name
 
         conn = sqlite3.connect(db_name)
         cur = conn.cursor()
 
         return conn, cur
 
-    def setup_table(self, table_script):
+    def create_table(self):
         '''
         Execute table creation script.
         '''
-        self.cur.execute(table_script)
-        self.conn.commit()
+        self.cur.execute(self.table_create_script)
+        # self.conn.commit()
 
     def setup_folder_structure(self):
         now = now_utc()
